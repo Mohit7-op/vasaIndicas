79159a188ae33a8dc73148eea646f840a0e66001
Mohit N <mohitn@techsprintglobal.com>
2025-06-06 14:02:00 +0530

    push theme code

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'vs-', // optional, only if you want to use a prefix
+  content: ["./**/*.liquid"], // fixed glob
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
